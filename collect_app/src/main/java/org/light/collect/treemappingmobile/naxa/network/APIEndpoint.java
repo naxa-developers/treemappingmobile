4ead437250ce040fba85809fdcbeffa366e869cb
samirdangal <samirdangal@gmail.com>
2019-08-30 14:42:08 +0545

    folder name change

--- collect_app/src/main/java/org/light/collect/treemappingmobile/naxa/network/APIEndpoint.java
+++ collect_app/src/main/java/org/light/collect/treemappingmobile/naxa/network/APIEndpoint.java
@@ -2,6 +2,6 @@ package org.light.collect.treemappingmobile.naxa.network;
 
 public class APIEndpoint {
 
-    public static final String BASE_URL = "https://utilitymaps.org/tree/ApiController/";
+    public static final String BASE_URL = "http://tree.utilitymaps.org/ApiController/";
     public static final String UPLOAD_FORM = "street_light_api";
 }
